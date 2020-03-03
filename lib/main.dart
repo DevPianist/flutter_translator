05ca35efc689ec58c82c0c14b73d6eba05d4065b
DevPianist <oscardelgadillodev@outlook.com>
2020-03-02 21:56:19 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import './ui/app.dart';
+
+void main() => runApp(MyApp());
---
+++ lib/ui/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/': (context) => HomeScreen(),
+      },
+    );
+  }
+}
